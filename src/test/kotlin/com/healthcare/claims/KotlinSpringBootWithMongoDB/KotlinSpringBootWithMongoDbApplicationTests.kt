0322f7eb92f048ffe22badb46f0f7d2f24e7b06c
kavitha edula <kavithaed@gmail.com>
2024-08-25 10:54:51 -0500

    commit two

---
+++ src/test/kotlin/com/healthcare/claims/KotlinSpringBootWithMongoDB/KotlinSpringBootWithMongoDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.healthcare.claims.KotlinSpringBootWithMongoDB
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootWithMongoDbApplicationTests {
+
+	/*@Test
+	fun contextLoads() {
+	}*/
+
+}
